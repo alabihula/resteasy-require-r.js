c585dace206baf47e7c2e1958bf4266187f45232
minxing <254332471@qq.com>
2016-07-01 18:03:51 +0800

    增加引用写的好的前端的一些知识的模块，用来时刻拜读

---
+++ src/main/webapp/webKnowledge/js/main.js
@@ -0,0 +1,34 @@
+require.config({
+    paths: {
+        jquery: '../../commons/js/jquery-1.12.4.min',
+        jqueryUI:'jquery-ui.min'
+    }
+});
+ 
+require(['jquery','jqueryUI'],function($) {
+	$('#tabs').tabs(
+			{
+//				active:1,//默认显示的标签
+				activate :function(e,ui) {
+					console.log(e);
+					console.log(ui);
+				}
+			}
+		).show();
+	
+//	$( "#tabs" ).tabs( "option", "active", 2 );
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
+
+
